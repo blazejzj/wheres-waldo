721459f65585783a9ef446d9b2fb426800e9c48a
blazejzj <opexrrator@gmail.com>
2025-06-15 17:24:50 +0200

    feat: started slightly with frontend main page, where you'll be able to choose an image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,28 @@
+import homeBg from "./assets/homeBg.jpg";
+import "../index.css";
+
 function App() {
-    return <h1 className="text-red-700">hello</h1>;
+    return (
+        <div
+            className="gap-5 w-full h-full bg-cover bg-center flex justify-center items-center flex-col"
+            style={{
+                backgroundImage: `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url(${homeBg})`,
+            }}
+        >
+            <h1 className="text-7xl uppercase luckyguy">
+                Where's Waldo{" "}
+                <span className="text-2xl text-gray-700">by blazejzj</span>
+            </h1>
+            <div className="w-[30rem] h-[300px] bg-white rounded-md flex flex-col items-center">
+                <h1>Choose any image to start!</h1>
+                <div>
+                    <li>Hello</li>
+                    <li>Hello2</li>
+                    <li>Hello3</li>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,8 @@
+function Home() {
+    function Greeting() {
+        return <h1>Welcome to Where's Waldo?!</h1>;
+    }
+    return;
+}
+
+export default Home;
