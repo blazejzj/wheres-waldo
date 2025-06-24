fb64fd0dc517eee6070a7e562680c2c07ed3db0a
blazejzj <opexrrator@gmail.com>
2025-06-24 12:25:40 +0200

    feat: bug fixes, complained about type mismatch...

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,42 @@
-const express = require("express");
-import { Request, Response } from "express";
-const cors = require("cors");
-require("dotenv").config();
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
+dotenv.config();
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
     res.json({ status: "backend up!" });
 });
 
-// For simpilicty we're not going to add controllers or routers because not many paths.
-
-app.post("/api/score", async (req: Request, res: Response) => {
+app.post("/api/score", async (req, res) => {
     const { name, time, imageId } = req.body;
-    if (!name || !time || !imageId)
-        return res.status(400).json({ error: "Missing fields." });
-
+    if (!name || !time || !imageId) {
+        res.status(400).json({ error: "Missing fields." });
+        return;
+    }
     const score = await prisma.score.create({
         data: { name, time, imageId },
     });
-
     res.json({ success: true, score });
 });
 
-app.get("/api/leaderboard", async (req: Request, res: Response) => {
-    const imageId = Number(req.query.imageId);
-    if (!imageId) return res.status(400).json({ error: "Missing imadeId" });
-
+app.get("/api/leaderboard", async (req, res) => {
+    const imageId = Number(req.query.imageId as string);
+    if (!imageId) {
+        res.status(400).json({ error: "Missing imageId" });
+        return;
+    }
     const scores = await prisma.score.findMany({
         where: { imageId },
         orderBy: { time: "asc" },
         take: 10,
     });
-
     res.json({ scores });
 });
 
